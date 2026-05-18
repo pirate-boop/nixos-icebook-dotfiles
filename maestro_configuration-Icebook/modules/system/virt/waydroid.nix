1e47149aa8b04b5c074138d6ea9929287d59b1fd
Kori <daniilignatev290@proton.me>
2026-05-18 19:00:29 +0300

    Enhance Waydroid configuration structure
    Refactor Waydroid configuration to include package and system packages.

--- maestro_configuration-Icebook/modules/system/virt/waydroid.nix
+++ maestro_configuration-Icebook/modules/system/virt/waydroid.nix
@@ -1,11 +1,22 @@
-{ ... }: {
-  virtualisation.waydroid.enable = true;
-  #networking.firewall.trustedIterfaces = ["waydroid0"];
+{ pkgs, ... }: {
+  # 1. Правильная структура включения Waydroid и указания пакета
+  virtualisation.waydroid = {
+    enable = true;
+    package = pkgs.waydroid-nftables; # Именно так мы указываем использовать nftables
+  };
+  # 2. systemPackages должен быть на верхнем уровне, не внутри waydroid!
+  environment.systemPackages = with pkgs; [
+    waydroid-helper
+  ];
+
+  networking.firewall.trustedInterfaces = ["waydroid0"];
+
   boot.kernel.sysctl = {
    "net.ipv4.ip_forward" = 1;
    "net.ipv4.conf.all.forwarding" = 1;
    "net.ipv6.conf.all.forwarding" = 1;
   };
+
   systemd.services.waydroid-container.serviceConfig = {
   Delegate = true;
   CPUAccounting = true;
