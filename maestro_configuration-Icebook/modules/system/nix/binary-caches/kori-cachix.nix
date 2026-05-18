aa45f21d700a8f45b4797153a66388e1dbc0fb78
Kori <daniilignatev290@proton.me>
2026-05-19 01:23:43 +0300

    Update trusted public key for kori-cachix

--- maestro_configuration-Icebook/modules/system/nix/binary-caches/kori-cachix.nix
+++ maestro_configuration-Icebook/modules/system/nix/binary-caches/kori-cachix.nix
@@ -1,6 +1,6 @@
 {
   nix.settings = {
     substituters = [ "https://kori-cachix.cachix.org" ];
-    trusted-public-keys = [ "kori-cachix.cachix.org-1:eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyNzc5MDBjYi1lMjcxLTQ0YzItYmIyMC0zZjA5MWFkNDU1NjciLCJzY29wZXMiOiJjYWNoZSJ9.7FvCkiPlOu1asF7NoQjtECyvCW2qVUnEMDFyiM-tibk" ];
+    trusted-public-keys = [ "kori-cachix.cachix.org-1:lvfvhLRZaSiGOD8HO3C3Ji2LHzixWRI2B2l0dyYoQ3g=" ];
   };
 }
