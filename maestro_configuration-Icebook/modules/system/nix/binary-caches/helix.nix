ee49e5b7b84ad64580d9ea2321ab90d3861942d2
Kori <daniilignatev290@proton.me>
2026-05-19 00:54:30 +0300

    Uncomment Helix configuration in helix.nix

--- maestro_configuration-Icebook/modules/system/nix/binary-caches/helix.nix
+++ maestro_configuration-Icebook/modules/system/nix/binary-caches/helix.nix
@@ -1,6 +1,6 @@
-# Раскомментируй если используешь Helix
-# { nix.settings = {
-#   substituters      = [ "https://helix.cachix.org" ];
-#   trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
-# }; }
+Раскомментируй если используешь Helix
+{ nix.settings = {
+    substituters      = [ "https://helix.cachix.org" ];
+    trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
+  }; }
 { }
