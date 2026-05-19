e95d25170ce9b087034904d5a1c443fac82223fd
Kori <daniilignatev290@proton.me>
2026-05-19 03:46:04 +0300

    Add pointerCursor configuration for home services

---
+++ maestro_configuration-Icebook/modules/home/home-services/pointerCursor.nix
@@ -0,0 +1,7 @@
+home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "FernBLZ";
+    size = 24;
+    package = inputs.anime-cursors.packages.${pkgs.system}.FernBLZ;
+  };
