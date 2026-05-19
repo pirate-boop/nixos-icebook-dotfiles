3522aab0969dc94b0245d81c6772cc84c6676f60
Kori <daniilignatev290@proton.me>
2026-05-19 21:22:06 +0300

    Update config-files.nix

--- maestro_configuration-Icebook/modules/home/home-xdg/config-files.nix
+++ maestro_configuration-Icebook/modules/home/home-xdg/config-files.nix
@@ -2,15 +2,16 @@
   xdg.configFile."niri/config.kdl".source = ../.dotfiles/.config/niri/config.kdl; xdg.configFile."niri/config.kdl".force = true;
   xdg.configFile."yazi/yazi.toml".source = ../.dotfiles/.config/yazi/yazi.toml;
   xdg.configFile."hypr/hyprlock.conf".source = ../.dotfiles/.config/hypr/hyprlock.conf;
-  xdg.configFile."yazi/plugins" = {
+
+ xdg.configFile."yazi/plugins" = {
     source = pkgs.symlinkJoin {
       name = "yazi-plugins";
       paths = [
         pkgs.yaziPlugins.mount
         pkgs.yaziPlugins.gitui
         pkgs.yaziPlugins.chmod
-        # Добавляй сюда остальные плагины из pkgs.yaziPlugins
       ];
     };
+    recursive = true;
   };
 }
