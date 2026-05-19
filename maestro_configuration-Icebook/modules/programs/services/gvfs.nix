6adbcedf60b70de8fe008f30c96cdde525f54d60
Kori <daniilignatev290@proton.me>
2026-05-19 05:57:50 +0300

    Add GIO_EXTRA_MODULES to session variables

--- maestro_configuration-Icebook/modules/programs/services/gvfs.nix
+++ maestro_configuration-Icebook/modules/programs/services/gvfs.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }: {
   services.gvfs.enable = true;
   services.udev.packages = [ pkgs.libmtp.out ];
+  environment.sessionVariables = {
+    GIO_EXTRA_MODULES = "${pkgs.gvfs}/lib/gio/modules";
+  };
 }
