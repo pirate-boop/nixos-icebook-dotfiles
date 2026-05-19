3b882fae72e1656dda5b851d67c96c00372ae8b1
Kori <daniilignatev290@proton.me>
2026-05-19 03:49:41 +0300

    Refactor pointerCursor configuration to use inputs

--- maestro_configuration-Icebook/modules/home/home-services/pointerCursor.nix
+++ maestro_configuration-Icebook/modules/home/home-services/pointerCursor.nix
@@ -1,7 +1,10 @@
-home.pointerCursor = {
+{ inputs, pkgs, ... }:
+{
+  home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
     name = "FernBLZ";
     size = 24;
     package = inputs.anime-cursors.packages.${pkgs.system}.FernBLZ;
   };
+}
