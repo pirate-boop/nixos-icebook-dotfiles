75f1a5deda2cbdf3c3451dd29ccabe968a4c76e9
Kori <daniilignatev290@proton.me>
2026-05-19 21:15:14 +0300

    Enhance xdg config with yazi plugins
    Added yazi plugins configuration to xdg settings.

--- maestro_configuration-Icebook/modules/home/home-xdg/config-files.nix
+++ maestro_configuration-Icebook/modules/home/home-xdg/config-files.nix
@@ -1,5 +1,16 @@
-{ ... }: {
+{ pkgs, ... }: {
   xdg.configFile."niri/config.kdl".source = ../.dotfiles/.config/niri/config.kdl; xdg.configFile."niri/config.kdl".force = true;
   xdg.configFile."yazi/yazi.toml".source = ../.dotfiles/.config/yazi/yazi.toml;
   xdg.configFile."hypr/hyprlock.conf".source = ../.dotfiles/.config/hypr/hyprlock.conf;
+  xdg.configFile."yazi/plugins" = {
+    source = pkgs.symlinkJoin {
+      name = "yazi-plugins";
+      paths = [
+        pkgs.yaziPlugins.mount
+        pkgs.yaziPlugins.gitui
+        pkgs.yaziPlugins.chmod
+        # Добавляй сюда остальные плагины из pkgs.yaziPlugins
+      ];
+    };
+  };
 }
