5840f6d7dbe4371876db58aa185a696c8274cb7a
Kori <daniilignatev290@proton.me>
2026-05-19 06:04:51 +0300

    Update gvfs.nix

--- maestro_configuration-Icebook/modules/programs/services/gvfs.nix
+++ maestro_configuration-Icebook/modules/programs/services/gvfs.nix
@@ -1,7 +1,4 @@
 { pkgs, ... }: {
   services.gvfs.enable = true;
   services.udev.packages = [ pkgs.libmtp.out ];
-  environment.sessionVariables = {
-    GIO_EXTRA_MODULES = "${pkgs.gvfs}/lib/gio/modules";
-  };
 }
