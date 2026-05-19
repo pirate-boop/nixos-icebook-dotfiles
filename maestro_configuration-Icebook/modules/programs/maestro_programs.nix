ae7a000bc1b2c2b592c4ced0984ca0d43cdfb9d5
Kori <daniilignatev290@proton.me>
2026-05-19 17:44:42 +0300

    Add wm-programs to maestro_programs.nix

--- maestro_configuration-Icebook/modules/programs/maestro_programs.nix
+++ maestro_configuration-Icebook/modules/programs/maestro_programs.nix
@@ -4,5 +4,6 @@
     ./gaming-programs/maestro_gaming-programs.nix
     ./desktop-programs/maestro_desktop-programs.nix
     ./services/maestro_services.nix
+    ./wm-programs/maestro_wm-programs.nix
   ];
 }
