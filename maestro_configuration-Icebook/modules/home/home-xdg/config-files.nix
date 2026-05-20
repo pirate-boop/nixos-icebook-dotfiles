273551cd1635a8421ebfeac09f46bf7e8d5945ba
Kori <daniilignatev290@proton.me>
2026-05-20 04:48:25 +0300

    Fix formatting for yazi keymap configuration

--- maestro_configuration-Icebook/modules/home/home-xdg/config-files.nix
+++ maestro_configuration-Icebook/modules/home/home-xdg/config-files.nix
@@ -7,7 +7,7 @@
 
   #--YAZI--
   xdg.configFile."yazi/yazi.toml".source = ../.dotfiles/.config/yazi/yazi.toml;
-  xdg.configFile."yazi/keymap.toml".source = ../.dotfiles/.config/yazi/keymap.toml
+  xdg.configFile."yazi/keymap.toml".source = ../.dotfiles/.config/yazi/keymap.toml;
   
   # xdg.configFile."yazi/plugins/mount.yazi".source = "${pkgs.yaziPlugins.mount}/share/yazi/plugins/mount";
   
