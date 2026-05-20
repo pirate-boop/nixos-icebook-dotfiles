0ca2237661e9df7fec3da15fc46c880897229c09
Kori <daniilignatev290@proton.me>
2026-05-20 07:44:35 +0300

    Configure automatic MTP mounting with GVFS
    Added automatic MTP mounting service configuration for GVFS.

--- maestro_configuration-Icebook/modules/programs/services/gvfs.nix
+++ maestro_configuration-Icebook/modules/programs/services/gvfs.nix
@@ -1,5 +1,21 @@
 { pkgs, config, ... }: {
   services.gvfs.enable = true;
   services.udev.packages = [ pkgs.libmtp.out ];
-  services.devmon.enable = true;
+
+  # Нативный автомат для MTP в окружении Niri
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ENV{ID_MTP_DEVICE}=="1", TAG+="systemd", ENV{SYSTEMD_USER_WANTED}+="mtp-automount.service"
+  '';
+
+  systemd.user.services.mtp-automount = {
+    description = "Автоматическое монтирование смартфонов (MTP) через GVFS";
+    script = ''
+      ${pkgs.coreutils}/bin/sleep 1
+      ${pkgs.glib}/bin/gio mount -m
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = false;
+    };
+  };
 }
