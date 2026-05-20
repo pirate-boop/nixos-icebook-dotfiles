d4b68723aa4547afac529b4eecb976a28af97de0
Kori <daniilignatev290@proton.me>
2026-05-20 09:58:59 +0300

    Refactor yazi plugins configuration for clarity

--- maestro_configuration-Icebook/modules/home/home-programs/yazi.plugins.nix
+++ maestro_configuration-Icebook/modules/home/home-programs/yazi.plugins.nix
@@ -1,37 +1,38 @@
 { pkgs, ... }:
+let
+  plugins = with pkgs.yaziPlugins; {
+    gvfs = gvfs;
+    diff = diff;
+    drag = drag;
+    glow = glow;
+    jjui = jjui;
+    nord = nord;
+    ouch = ouch;
+    sudo = sudo;
+    yafg = yafg;
+    chmod = chmod;
+    dupes = dupes;
+    gitui = gitui;
+    mount = mount;
+    piper = piper;
+    rsync = rsync;
+    restore = restore;
+    yatline = yatline;
+    mime-ext = mime-ext;
+    bookmarks = bookmarks;
+    smart-enter = smart-enter;
+    nav-parent-panel = nav-parent-panel;
+    close-and-restore-tab = close-and-restore-tab;
+    toggle-pane = toggle-pane;
+    rich-preview = rich-preview;
+    smart-filter = smart-filter;
+    wl-clipboard = wl-clipboard;
+  };
+in
 {
   programs.yazi = {
     enable = true;
-
-    plugins = with pkgs.yaziPlugins; {
-      gvfs = gvfs;
-      diff = diff;
-      drag = drag;
-      glow = glow;
-      jjui = jjui;
-      nord = nord;
-      ouch = ouch;
-      sudo = sudo;
-      yafg = yafg;
-      chmod = chmod;
-      dupes = dupes;
-      gitui = gitui;
-      mount = mount;
-      piper = piper;
-      rsync = rsync;
-      restore = restore;
-      yatline = yatline;
-      mime-ext = mime-ext;
-      bookmarks = bookmarks;
-      smart-enter = smart-enter;
-      nav-parent-panel = nav-parent-panel;
-      close-and-restore-tab = close-and-restore-tab;
-      toggle-pane = toggle-pane;
-      rich-preview = rich-preview;
-      smart-filter = smart-filter;
-      wl-clipboard = wl-clipboard;
-    };
-
+    inherit plugins;
     initLua = builtins.concatStringsSep "\n" (
       map (name: ''require("${name}"):setup()'') (builtins.attrNames plugins)
     );
