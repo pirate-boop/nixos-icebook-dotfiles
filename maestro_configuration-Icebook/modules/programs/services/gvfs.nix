d58f5d93c216c11d1d217820fff96deb3a6a9694
Kori <daniilignatev290@proton.me>
2026-05-20 10:55:38 +0300

    Refactor gvfs service configuration

--- maestro_configuration-Icebook/modules/programs/services/gvfs.nix
+++ maestro_configuration-Icebook/modules/programs/services/gvfs.nix
@@ -1,5 +1,8 @@
 { pkgs, config, ... }: {
-  services.gvfs.enable = true;
+  services.gvfs = {
+    enable = true;
+    package = pkgs.gvfs.override { mtp = true; };
+  };
   services.udev.packages = [ pkgs.libmtp.out ];
   services.devmon.enable = true;
 }
