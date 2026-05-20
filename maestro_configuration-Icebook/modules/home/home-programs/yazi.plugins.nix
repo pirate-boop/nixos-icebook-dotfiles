a162940a3ee3731755ea1df49ba5c5e749612acb
Kori <daniilignatev290@proton.me>
2026-05-20 04:39:30 +0300

    Add yazi.plugins.nix configuration file

---
+++ maestro_configuration-Icebook/modules/home/home-programs/yazi.plugins.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+
+{
+  programs.yazi = {
+    enable = true;
+
+    plugins = with pkgs.yaziPlugins; {
+      gvfs = gvfs;
+
+      diff = diff;
+      drag = drag;
+      glow = glow;
+      jjui = jjui;
+      nord = nord;
+
+      ouch = ouch;
+      sudo = sudo;
+      yafg = yafg;
+
+      chmod = chmod;
+      dupes = dupes;
+      gitui = gitui;
+
+      mount = mount;
+      piper = piper;
+      rsync = rsync;
+      restore = restore;
+
+      yatline = yatline;
+      mime-ext = mime-ext;
+
+      bookmarks = bookmarks;
+      smart-enter = smart-enter;
+
+      nav-parent-panel = nav-parent-panel;
+      close-and-restore-tab = close-and-restore-tab;
+      toggle-pane = toggle-pane;
+
+      rich-preview = rich-preview;
+      smart-filter = smart-filter;
+      wl-clipboard = wl-clipboard;
+    };
+  };
+}
