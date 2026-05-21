8a4d94f3f7b0a155bb2fc677062aedeea78e75d3
Kori <daniilignatev290@proton.me>
2026-05-21 05:32:26 +0300

    Create Flatpak configuration in flatpak.nix
    Add Flatpak configuration with repositories and utilities.

---
+++ maestro_configuration-Icebook/modules/programs/desktop-programs/flatpak.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }: {
+  # Включаем системный демон Flatpak
+  services.flatpak.enable = true;
+
+  # Базовые утилиты и графический магазин
+  environment.systemPackages = with pkgs; [
+    flatpak
+    flatseal        # Графический редактор прав
+    gnome-software  # Графический магазин приложений
+  ];
+
+  # Порталы для корректной работы под Wayland/Niri
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-wlr
+    ];
+    config.common.default = "*";
+  };
+
+  # Декларативное добавление ВСЕХ основных репозиториев
+  systemd.services.configure-flatpak-repos = {
+    description = "Автоматическое добавление репозиториев Flatpak";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      # 1. Основной стабильный Flathub (Steam, Zoom, Discord и т.д.)
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+      # 2. Flathub Beta (резервный репозиторий с бета-версиями софта)
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub-beta https://dl.flathub.org/repo/flathub-beta.flatpakrepo
+
+      # 3. GNOME Nightly (официальный репозиторий приложений GNOME)
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+    '';
+  };
+}
