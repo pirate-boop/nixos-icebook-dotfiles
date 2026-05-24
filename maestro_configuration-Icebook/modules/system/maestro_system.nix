548d0400f61e3fafb987dc784a81ab06159bcc86
Kori <daniilignatev290@proton.me>
2026-05-24 22:31:48 +0300

    Reformat imports in maestro_system.nix

--- maestro_configuration-Icebook/modules/system/maestro_system.nix
+++ maestro_configuration-Icebook/modules/system/maestro_system.nix
@@ -6,13 +6,14 @@
 #****************************************************************#
 { config, pkgs, inputs, ... }: {
   imports = [
-    ./core/maestro_core.nix # загрузчик, ядро, kernel params
-    ./fonts/maestro_fonts.nix # пользователь kori
-    ./hardware/maestro_hardware.nix # sysctl, PAM, sudo
-    ./localization/maestro_localization.nix # таймзона, локаль
-    ./network/maestro_networking.nix # NetworkManager
-    ./nix/maestro.nix # nix daemon + flake settings
-    ./shell/maestro_shell.nix # ZSH + oh-my-zsh
-    ./virt/maestro_virt.nix # libvirtd, QEMU, Waydroid
+    ./core/maestro_core.nix                            # загрузчик, ядро, kernel params
+    ./fonts/maestro_fonts.nix                          # пользователь kori
+    ./hardware/maestro_hardware.nix                    # sysctl, PAM, sudo
+    ./localization/maestro_localization.nix            # таймзона, локаль
+    ./network/maestro_networking.nix                   # NetworkManager
+    ./nix/maestro.nix                                  # nix daemon + flake settings
+    ./shell/maestro_shell.nix                          # ZSH + oh-my-zsh
+    ./virt/maestro_virt.nix                            # libvirtd, QEMU, Waydroid
+    ./keyboard-settings/maestro_keyboard-settings.nix  # keyboard-settings
   ];
 }
