891ee3582ac7362a42241fb1ce866174706fd678
Kori <daniilignatev290@proton.me>
2026-06-15 00:13:33 +0000

    Add packages for wireless auditing and MAC address hiding

--- maestro_configuration-Icebook/modules/system_packages/offensive-wifitools.nix
+++ maestro_configuration-Icebook/modules/system_packages/offensive-wifitools.nix
@@ -1,1 +1,25 @@
+{ config, pkgs, ... }:
 
+{
+  # 1. Пакеты для беспроводного аудита
+  environment.systemPackages = with pkgs; [
+    iw
+    wirelesstools
+    aircrack-ng
+    wifite2
+    hcxtools
+    hcxdumptool
+    hashcat
+    
+    # Дополнительный софт, что тебе посоветовали:
+    reaver
+    pixiewps
+    bettercap
+  ];
+
+  # 2. Параноидальный режим: Автоматическое сокрытие MAC-адреса
+  networking.networkmanager = {
+    wifi.macAddress = "random";       # Смена MAC при каждом новом подключении к сети
+    wifi.scanRandMacAddress = true;   # Маскировать MAC даже во время обычного сканирования эфира
+  };
+}
