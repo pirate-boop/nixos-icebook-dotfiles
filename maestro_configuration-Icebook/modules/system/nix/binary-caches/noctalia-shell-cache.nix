8b08f9b847751757e51779ee30a5ef0588001dd6
Kori <daniilignatev290@proton.me>
2026-06-15 22:48:02 +0000

    Wrap nix settings in an additional block

--- maestro_configuration-Icebook/modules/system/nix/binary-caches/noctalia-shell-cache.nix
+++ maestro_configuration-Icebook/modules/system/nix/binary-caches/noctalia-shell-cache.nix
@@ -1,4 +1,6 @@
-nix.settings = {
-  extra-substituters = [ "https://noctalia.cachix.org" ];
-  extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-};
+{
+  nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
+}
