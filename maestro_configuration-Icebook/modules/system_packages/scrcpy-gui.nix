8c27e3a12a7c5c5e1aee1897a9ede4844451b009
Kori <daniilignatev290@proton.me>
2026-06-15 00:39:40 +0000

    Add scrcpy-gui package to system packages

--- maestro_configuration-Icebook/modules/system_packages/scrcpy-gui.nix
+++ maestro_configuration-Icebook/modules/system_packages/scrcpy-gui.nix
@@ -1,1 +1,8 @@
+{ config, pkgs, inputs, ... }:
 
+{
+  # Забираем дефолтный пакет из инпута scrcpy-gui под  архитектуру (x86_64-linux)
+  environment.systemPackages = [
+    inputs.scrcpy-gui.packages.${pkgs.system}.default
+  ];
+}
