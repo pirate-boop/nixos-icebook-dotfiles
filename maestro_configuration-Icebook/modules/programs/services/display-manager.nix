dd9dbff831b635c8661683aee2237e2b47862203
Kori <daniilignatev290@proton.me>
2026-06-17 20:04:28 +0000

    Update display-manager.nix

--- maestro_configuration-Icebook/modules/programs/services/display-manager.nix
+++ maestro_configuration-Icebook/modules/programs/services/display-manager.nix
@@ -4,15 +4,19 @@ let
   custom-sddm-astronaut = pkgs.sddm-astronaut.override {
     embeddedTheme = "hyprland_kath";
   };
-
 in {
+  # Добавляем тему в системные пакеты, чтобы SDDM её видел
+  environment.systemPackages = [
+    custom-sddm-astronaut
+  ];
+
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true; # Обязательно для SDDM, если выключен X-сервер
+    wayland.enable = true;
     theme = "sddm-astronaut-theme";
-    
+
+    # Здесь оставляем только зависимости
     extraPackages = [
-      custom-sddm-astronaut
       pkgs.kdePackages.qtmultimedia
     ];
   };
