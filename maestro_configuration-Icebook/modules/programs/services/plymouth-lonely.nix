cc72ee3b062a2a1a6abf0a585442b2db0385d149
Kori <daniilignatev290@proton.me>
2026-06-20 02:57:56 +0300

    Add Plymouth theme configuration for 'lonely'

---
+++ maestro_configuration-Icebook/modules/programs/services/plymouth-lonely.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  boot.plymouth = {
+    enable = true;
+    theme = "lonely";
+    themePackages = [
+      (pkgs.stdenv.mkDerivation {
+        pname = "plymouth-theme-lonely";
+        version = "1.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "AntarcDev";
+          repo = "lonely";
+          rev = "main";
+          # Временная заглушка. При сборке Nix выдаст ошибку и покажет правильный хэш (SRI), который нужно будет вставить сюда.
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+
+        # Копируем всё содержимое репозитория прямо в системную директорию тем Plymouth
+        installPhase = ''
+          mkdir -p $out/share/plymouth/themes/lonely
+          cp -r * $out/share/plymouth/themes/lonely/
+        '';
+      })
+    ];
+  };
+
+  # Ранняя инициализация видеодрайвера AMD, чтобы анимация загрузки подхватывала родное разрешение экрана сразу, без морганий
+  hardware.amdgpu.initrd.enable = true;
+}
