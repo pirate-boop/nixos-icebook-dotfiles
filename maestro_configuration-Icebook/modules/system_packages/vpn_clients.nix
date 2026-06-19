1e8b66b3fcc8b2da4b1e8221e3724461dba29f0f
Kori <daniilignatev290@proton.me>
2026-06-20 01:02:11 +0300

    Update vpn_clients.nix to include geo-data configuration
    Add automatic download and linking of geo-data for daed

--- maestro_configuration-Icebook/modules/system_packages/vpn_clients.nix
+++ maestro_configuration-Icebook/modules/system_packages/vpn_clients.nix
@@ -9,4 +9,10 @@
     linux-wifi-hotspot
     daed
   ];
+
+  # Автоматическое скачивание и линкование баз гео-данных для daed
+  environment.etc = {
+    "dae-wing/geoip.dat".source = "${pkgs.v2ray-geoip}/share/v2ray/geoip.dat";
+    "dae-wing/geosite.dat".source = "${pkgs.v2ray-domain-list-community}/share/v2ray/geosite.dat";
+  };
 }
