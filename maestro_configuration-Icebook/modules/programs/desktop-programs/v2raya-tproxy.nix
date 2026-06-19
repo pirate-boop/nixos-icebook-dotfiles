5a09858286ed786ebaade0d1c6a9055358fa31ba
Kori <daniilignatev290@proton.me>
2026-06-19 21:15:44 +0300

    Update v2raya-tproxy.nix

--- maestro_configuration-Icebook/modules/programs/desktop-programs/v2raya-tproxy.nix
+++ maestro_configuration-Icebook/modules/programs/desktop-programs/v2raya-tproxy.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # 1. Включаем службу v2rayA на движке xray
@@ -15,8 +15,8 @@
 
   # 3. Разрешаем пересылку пакетов на уровне ядра (без этого TProxy сдохнет)
   boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
+    "net.ipv4.ip_forward" = lib.mkForce 1;
+    "net.ipv6.conf.all.forwarding" = lib.mkForce 1;
   };
 
   # 4. Даем изолированной службе доступ к сетевым бинарникам
