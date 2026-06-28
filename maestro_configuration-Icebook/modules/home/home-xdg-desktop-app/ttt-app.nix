992eee7583f3c684b075796ef7287ef9fab9e780
Kori <daniilignatev290@proton.me>
2026-06-28 13:12:07 +0000

    Add desktop entry for WezTerm with ttt command

--- maestro_configuration-Icebook/modules/home/home-xdg-desktop-app/ttt-app.nix
+++ maestro_configuration-Icebook/modules/home/home-xdg-desktop-app/ttt-app.nix
@@ -1,1 +1,17 @@
+{ config, pkgs, ... }:
 
+{
+  xdg.desktopEntries = {
+    # Имя ярлыка в системе (будет ttt-wezterm.desktop)
+    ttt-wezterm = {
+      name = "ttt (WezTerm)";
+      
+      # Запуск WezTerm с выполнением команды ttt и пробросом пути к файлу (%f)
+      exec = "wezterm start -- ttt %f"; 
+      
+      terminal = false; # false, потому что wezterm сам является терминалом
+      categories = [ "Utility" "TextEditor" ];
+      mimeType = [ "text/plain" ];
+    };
+  };
+}
