16f58453ab89e44dc7530a1f308221f04ff6bbc1
Kori <daniilignatev290@proton.me>
2026-07-13 02:22:53 +0300

    Comment out wm-program-mango.nix and add dms-shell

--- maestro_configuration-Icebook/modules/programs/wm-programs/maestro_wm-programs.nix
+++ maestro_configuration-Icebook/modules/programs/wm-programs/maestro_wm-programs.nix
@@ -2,6 +2,7 @@
   imports = [
     ./wm-program-niri.nix
     #./wm-program-hyprland.nix щас он мне будет нахуй не нужон
-    ./wm-program-mango.nix
+    #./wm-program-mango.nix
+    ./wm-program-dms-shell.nix
   ];
 }
