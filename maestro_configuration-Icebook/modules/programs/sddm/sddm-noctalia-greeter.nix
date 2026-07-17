5c707036c4692b103a890476db958f757d8fe9ae
Kori <daniilignatev290@proton.me>
2026-07-17 03:10:01 +0000

    Update sddm-noctalia-greeter.nix

--- maestro_configuration-Icebook/modules/programs/sddm/sddm-noctalia-greeter.nix
+++ maestro_configuration-Icebook/modules/programs/sddm/sddm-noctalia-greeter.nix
@@ -1,13 +1,18 @@
-programs.noctalia-greeter = {
-  enable = true;
-  settings = {
-    cursor = {
-      theme = "FernBLZ";
-      size = 24;
-      path = "${inputs.anime-cursors.packages.${pkgs.system}.FernBLZ}/share/icons";
-    };
-    keyboard = {
-      layout = "us,ru";
+{ inputs, pkgs, ... }:
+{
+  imports = [ inputs.noctalia-greeter.nixosModules.default ];
+
+  programs.noctalia-greeter = {
+    enable = true;
+    settings = {
+      cursor = {
+        theme = "FernBLZ";
+        size = 24;
+        path = "${inputs.anime-cursors.packages.${pkgs.system}.FernBLZ}/share/icons";
+      };
+      keyboard = {
+        layout = "us,ru";
+      };
     };
   };
-};
+}
