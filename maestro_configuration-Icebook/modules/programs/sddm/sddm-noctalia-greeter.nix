e3ec1140ebafdafcf14e19b81f65093dc321b13c
Kori <daniilignatev290@proton.me>
2026-07-17 02:15:56 +0000

    Configure noctalia-greeter with custom settings
    Added configuration for noctalia-greeter with cursor and keyboard settings.

--- maestro_configuration-Icebook/modules/programs/sddm/sddm-noctalia-greeter.nix
+++ maestro_configuration-Icebook/modules/programs/sddm/sddm-noctalia-greeter.nix
@@ -1,1 +1,17 @@
+{ inputs, pkgs, ... }:
+{
+  imports = [ inputs.noctalia-greeter.nixosModules.default ];
 
+  programs.noctalia-greeter = {
+    enable = true;
+    settings = {
+      cursor = {
+        theme = "Bibata-Modern-Ice";
+        size = 24;
+      };
+      keyboard = {
+        layout = "us,ru";
+      };
+    };
+  };
+}
