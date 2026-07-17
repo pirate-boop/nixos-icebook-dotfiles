ea5b261cfc0da5735de115c72ba24c6201d19e45
Kori <daniilignatev290@proton.me>
2026-07-17 02:48:32 +0000

    Refactor noctalia and add noctalia-greeter

--- flake.nix
+++ flake.nix
@@ -15,10 +15,11 @@
     waydroid-script     = { url = "github:casualsnek/waydroid_script"; inputs.nixpkgs.follows = "nixpkgs"; };
     anime-cursors.url   = "github:pirate-boop/anime-cursors";
     scrcpy-gui          = { url = "github:kil0bit-kb/scrcpy-gui"; inputs.nixpkgs.follows = "nixpkgs"; };
-    noctalia.url        = "github:noctalia-dev/noctalia";
+    noctalia            = { url = "github:noctalia-dev/noctalia"; inputs.nixpkgs.follows = "nixpkgs"; }; 
+    noctalia-greeter    = { url = "github:noctalia-dev/noctalia-greeter"; inputs.nixpkgs.follows = "nixpkgs"; };
     ttt.url             = "github:eugenioenko/ttt";
     mangowm             = { url = "github:mangowm/mango"; inputs.nixpkgs.follows = "nixpkgs"; };
-    dms =                 { url = "github:AvengeMedia/DankMaterialShell/stable"; inputs.nixpkgs.follows = "nixpkgs"; };
+    dms                 = { url = "github:AvengeMedia/DankMaterialShell/stable"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = inputs@{ flake-parts, ... }:
