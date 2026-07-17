489ac384f3e7d4daf397839665fb764120a8a401
Kori <daniilignatev290@proton.me>
2026-07-17 02:20:37 +0000

    Update noctalia-greeter cursor theme to FernBLZ

--- maestro_configuration-Icebook/modules/programs/sddm/sddm-noctalia-greeter.nix
+++ maestro_configuration-Icebook/modules/programs/sddm/sddm-noctalia-greeter.nix
@@ -1,17 +1,13 @@
-{ inputs, pkgs, ... }:
-{
-  imports = [ inputs.noctalia-greeter.nixosModules.default ];
-
-  programs.noctalia-greeter = {
-    enable = true;
-    settings = {
-      cursor = {
-        theme = "Bibata-Modern-Ice";
-        size = 24;
-      };
-      keyboard = {
-        layout = "us,ru";
-      };
+programs.noctalia-greeter = {
+  enable = true;
+  settings = {
+    cursor = {
+      theme = "FernBLZ";
+      size = 24;
+      path = "${inputs.anime-cursors.packages.${pkgs.system}.FernBLZ}/share/icons";
+    };
+    keyboard = {
+      layout = "us,ru";
     };
   };
-}
+};
