2aa396e335cfcca643ee713f91b366fdb81ce793
Kori <daniilignatev290@proton.me>
2026-07-17 02:37:48 +0000

    Update lock-session-kori.nix

--- maestro_configuration-Icebook/modules/home/home-programs/lock-session-kori.nix
+++ maestro_configuration-Icebook/modules/home/home-programs/lock-session-kori.nix
@@ -3,7 +3,7 @@
   home.packages = [
     (pkgs.writeShellScriptBin "lock-session-kori" ''
       ${pkgs.niri}/bin/niri msg action switch-layout 0
-      dms ipc call lock lock
+      noctalia msg session lock
     '')
   ];
 }
