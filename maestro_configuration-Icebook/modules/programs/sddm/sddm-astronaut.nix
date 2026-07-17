ca887fc4b417dcf2e7a8fed8ae04a7e362a17ea2
Kori <daniilignatev290@proton.me>
2026-07-17 02:13:59 +0000

    Configure SDDM with custom astronaut theme
    Add custom SDDM astronaut theme and dependencies.

--- maestro_configuration-Icebook/modules/programs/sddm/sddm-astronaut.nix
+++ maestro_configuration-Icebook/modules/programs/sddm/sddm-astronaut.nix
@@ -1,1 +1,23 @@
+{ pkgs, ... }:
 
+let
+  custom-sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "hyprland_kath";
+  };
+in {
+  # Добавляем тему в системные пакеты, чтобы SDDM её видел
+  environment.systemPackages = [
+    custom-sddm-astronaut
+  ];
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "sddm-astronaut-theme";
+
+    # Здесь оставляем только зависимости
+    extraPackages = [
+      pkgs.kdePackages.qtmultimedia
+    ];
+  };
+}
