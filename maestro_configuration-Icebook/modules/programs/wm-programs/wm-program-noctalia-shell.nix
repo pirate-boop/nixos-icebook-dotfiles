54f4c0a7c37014ebd5415ab9e44b7438926cd524
Kori <daniilignatev290@proton.me>
2026-07-17 01:48:03 +0000

    Update wm-program-noctalia-shell.nix to use inputs

--- maestro_configuration-Icebook/modules/programs/wm-programs/wm-program-noctalia-shell.nix
+++ maestro_configuration-Icebook/modules/programs/wm-programs/wm-program-noctalia-shell.nix
@@ -1,1 +1,7 @@
-
+{ inputs, pkgs, ... }: # Убедись, что на входе в файл есть inputs!
+{
+  environment.systemPackages = [
+    # Вместо старого pkgs.noctalia-qs пишем это:
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
+}
