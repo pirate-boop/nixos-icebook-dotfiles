bb3daf670bf72c7516207e8ffbf26d04bc8a1e15
Kori <daniilignatev290@proton.me>
2026-07-17 04:01:47 +0000

    Disable wm-program-dms-shell import
    Comment out the import for wm-program-dms-shell.nix

--- maestro_configuration-Icebook/modules/programs/wm-programs/maestro_wm-programs.nix
+++ maestro_configuration-Icebook/modules/programs/wm-programs/maestro_wm-programs.nix
@@ -1,7 +1,7 @@
 { ... }: {
   imports = [
     ./wm-program-niri.nix
-    ./wm-program-dms-shell.nix
+    #./wm-program-dms-shell.nix
     #./wm-program-hyprland.nix щас он мне будет нахуй не нужон
     #./wm-program-mango.nix
     ./wm-program-noctalia-shell.nix
