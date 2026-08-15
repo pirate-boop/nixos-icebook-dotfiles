68ca073895de41ead7445b4cc42f2b780fd73a9b
Kori <daniilignatev290@proton.me>
2026-08-15 18:14:22 +0300

    Disable fptn-vpn and onlyoffice packages temporarily
    Commented out imports and onlyoffice package due to issues.

--- pkgs/maestro_custom-packages.nix
+++ pkgs/maestro_custom-packages.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./fptn-vpn.nix ];
+  # disabled-by-restore: fptn-vpn.nix написан как callPackage-функция, а подключён модулем.
+  # Вернуть после починки шапки файла (см. мой чат).
+  # imports = [ ./fptn-vpn.nix ];
 
   environment.systemPackages = [
     (pkgs.callPackage ./zen-browser.nix { })
     (pkgs.callPackage ./noutube-desktop.nix { })
-    (pkgs.callPackage ./onlyoffice.nix { })
+    # disabled-by-restore: пустой hash + wrapGAppsHook3. Вернуть после:
+    # 1) в nativeBuildInputs wrapGAppsHook3 -> wrapGAppsHook
+    # 2) hash вставить через nix-prefetch-url
+    # (pkgs.callPackage ./onlyoffice.nix { })
   ];
 }
