68b5264010e8015824364eab6ccd61972c7738cb
Kori <daniilignatev290@proton.me>
2026-08-15 14:57:33 +0300

    Add custom packages to Nix configuration

--- configuration.nix
+++ configuration.nix
@@ -2,5 +2,6 @@
   imports = [
     ./hardware-configuration.nix                   # генерируется nixos-generate-config
     ./maestro_configuration-Icebook/igniter.nix
+    ./pkgs/maestro_custom-packages.nix
   ];
 }
