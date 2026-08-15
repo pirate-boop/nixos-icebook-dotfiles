a04fbb8a21c7d47f15624c0cbd41149162768d8d
Kori <daniilignatev290@proton.me>
2026-08-15 14:55:54 +0300

    Update maestro_custom-packages.nix

--- pkgs/maestro_custom-packages.nix
+++ pkgs/maestro_custom-packages.nix
@@ -1,1 +1,9 @@
+{ pkgs, ... }:
 
+{
+  environment.systemPackages = [
+    (pkgs.callPackage ../pkgs/zen-browser.nix { })
+    (pkgs.callPackage ../pkgs/noutube-desktop.nix { })
+    (pkgs.callPackage ../pkgs/onlyoffice.nix { })
+  ];
+}
