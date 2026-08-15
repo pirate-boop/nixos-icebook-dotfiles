289e37676edbaac5f0e19025cb09ec0d3a952865
Kori <daniilignatev290@proton.me>
2026-08-15 16:58:22 +0300

    Update maestro_custom-packages.nix

--- pkgs/maestro_custom-packages.nix
+++ pkgs/maestro_custom-packages.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
 
-let
-  fptn = pkgs.callPackage ./fptn-vpn.nix { };
-in
-fptn // {
-  environment.systemPackages = fptn.environment.systemPackages ++ [
+{
+  imports = [ ./fptn-vpn.nix ];
+
+  environment.systemPackages = [
     (pkgs.callPackage ./zen-browser.nix { })
     (pkgs.callPackage ./noutube-desktop.nix { })
     (pkgs.callPackage ./onlyoffice.nix { })
