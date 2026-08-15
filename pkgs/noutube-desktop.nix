28bcfc9101e8a6cbb7e12593290e7a6f63e9d37c
Kori <daniilignatev290@proton.me>
2026-08-15 09:41:51 +0300

    Add hash for NouTube Desktop AppImage

--- pkgs/noutube-desktop.nix
+++ pkgs/noutube-desktop.nix
@@ -6,7 +6,7 @@ appimageTools.wrapType2 rec {
 
   src = fetchurl {
     url = "https://github.com/nonbili/NouTube-Desktop/releases/download/v${version}/NouTube-${version}.AppImage";
-    hash = "";
+    hash = "sha256:a8d5ce3f54655107fb60932f1ad01c130c70ca588bca2758308a9d0f57285dc3";
   };
 
   extraPkgs = pkgs: with pkgs; [
