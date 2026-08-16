6deaa9d3c0f48df9dd10e840e4fb42cd94940614
JJ_EMPTY_STRING <JJ_EMPTY_STRING>
2026-08-16 15:50:03 +0300

    local fixes: options gaming fix=3tdddhomedfayuseerwqdsvsvsff34knnkkefwdw1fffrdfddcf

--- maestro_configuration-Icebook/modules/modules-deriv-packages/zen-browser.nix
+++ maestro_configuration-Icebook/modules/modules-deriv-packages/zen-browser.nix
@@ -1,107 +1,73 @@
-{ lib
-, stdenv
-, fetchurl
-, autoPatchelfHook
-, makeWrapper
-, wrapGAppsHook
-, alsa-lib
-, atk
-, cairo
-, cups
-, dbus
-, dbus-glib
-, ffmpeg
-, fontconfig
-, freetype
-, gdk-pixbuf
-, glib
-, gtk3
-, libglvnd
-, libjack2
-, libkrb5
-, libnotify
-, libpulseaudio
-, libdrm
-, mesa
-, nspr
-, nss
-, pango
-, pipewire
-, udev
-, xorg
-, wayland
-, libwebp
-, libevent
-, libvpx
-, icu
-, sqlite
-, gtk4
-, ...
-}:
+{ pkgs, ... }:
 
-stdenv.mkDerivation rec {
-  pname = "zen-browser";
-  version = "1.21.14b";
+{
+  environment.systemPackages = [
+    (pkgs.stdenv.mkDerivation rec {
+      pname = "zen-browser";
+      version = "1.21.14b";
 
-  src = fetchurl {
-    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-  };
+      src = pkgs.fetchurl {
+        url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
+        hash = "sha256:b3a5bb782d9403dce8cf4cde3d61daed64548568058862eaeb9e045c212b7ee0";
+      };
 
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper wrapGAppsHook ];
+      nativeBuildInputs = with pkgs; [
+        autoPatchelfHook makeWrapper wrapGAppsHook
+      ];
 
-  buildInputs = [
-    stdenv.cc.cc.lib alsa-lib atk cairo cups dbus dbus-glib ffmpeg
-    fontconfig freetype gdk-pixbuf glib gtk3 libglvnd libjack2
-    libkrb5 libnotify libpulseaudio libdrm mesa nspr nss pango
-    pipewire udev wayland libwebp libevent libvpx icu sqlite gtk4
-    xorg.libX11 xorg.libXcomposite xorg.libXdamage xorg.libXext
-    xorg.libXfixes xorg.libXrandr xorg.libxcb xorg.libxshmfence
-    xorg.libXtst
+      buildInputs = with pkgs; [
+        stdenv.cc.cc.lib alsa-lib atk cairo cups dbus dbus-glib ffmpeg
+        fontconfig freetype gdk-pixbuf glib gtk3 libglvnd libjack2
+        libkrb5 libnotify libpulseaudio libdrm mesa nspr nss pango
+        pipewire udev wayland libwebp libevent libvpx icu sqlite gtk4
+        xorg.libX11 xorg.libXcomposite xorg.libXdamage xorg.libXext
+        xorg.libXfixes xorg.libXrandr xorg.libxcb xorg.libxshmfence
+        xorg.libXtst
+      ];
+
+      sourceRoot = ".";
+      dontConfigure = true;
+      dontBuild = true;
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/lib/zen-browser
+        cp -r zen/* $out/lib/zen-browser/ 2>/dev/null || cp -r * $out/lib/zen-browser/
+
+        mkdir -p $out/bin
+        makeWrapper $out/lib/zen-browser/zen $out/bin/zen \
+          --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath buildInputs}" \
+          --set MOZ_ENABLE_WAYLAND 1 \
+          --set MOZ_USE_XINPUT2 1
+
+        mkdir -p $out/share/applications
+        cat > $out/share/applications/zen-browser.desktop <<EOF
+        [Desktop Entry]
+        Name=Zen Browser
+        Exec=$out/bin/zen %u
+        Icon=zen-browser
+        Type=Application
+        Categories=Network;WebBrowser;
+        MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;
+        EOF
+
+        mkdir -p $out/share/icons/hicolor/128x128/apps
+        for size in 16 32 48 64 128; do
+          icon="$out/lib/zen-browser/browser/chrome/icons/default/default''${size}.png"
+          [ -f "$icon" ] && cp "$icon" "$out/share/icons/hicolor/''${size}x''${size}/apps/zen-browser.png"
+        done
+
+        runHook postInstall
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Zen Browser";
+        homepage = "https://zen-browser.app/";
+        license = licenses.mpl20;
+        platforms = [ "x86_64-linux" ];
+        sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+      };
+    })
   ];
-
-  sourceRoot = ".";
-
-  dontConfigure = true;
-  dontBuild = true;
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/lib/zen-browser
-    cp -r zen/* $out/lib/zen-browser/ 2>/dev/null || cp -r * $out/lib/zen-browser/
-
-    mkdir -p $out/bin
-    makeWrapper $out/lib/zen-browser/zen $out/bin/zen \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
-      --set MOZ_ENABLE_WAYLAND 1 \
-      --set MOZ_USE_XINPUT2 1
-
-    mkdir -p $out/share/applications
-    cat > $out/share/applications/zen-browser.desktop <<EOF
-    [Desktop Entry]
-    Name=Zen Browser
-    Exec=$out/bin/zen %u
-    Icon=zen-browser
-    Type=Application
-    Categories=Network;WebBrowser;
-    MimeType=text/html;text/xml;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;
-    EOF
-
-    mkdir -p $out/share/icons/hicolor/128x128/apps
-    for size in 16 32 48 64 128; do
-      icon="$out/lib/zen-browser/browser/chrome/icons/default/default''${size}.png"
-      [ -f "$icon" ] && cp "$icon" "$out/share/icons/hicolor/''${size}x''${size}/apps/zen-browser.png"
-    done
-
-    runHook postInstall
-  '';
-
-  meta = with lib; {
-    description = "Zen Browser";
-    homepage = "https://zen-browser.app/";
-    license = licenses.mpl20;
-    platforms = [ "x86_64-linux" ];
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-  };
 }
