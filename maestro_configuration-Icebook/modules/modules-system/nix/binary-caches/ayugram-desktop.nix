aa76ca6d48e9b34d62374e251c4d0c473ff17a55
JJ_EMPTY_STRING <JJ_EMPTY_STRING>
2026-08-16 13:00:32 +0300

    local fixes: options gaming fix=3tdddhomedfayuseerwqds

--- maestro_configuration-Icebook/modules/modules-system/nix/binary-caches/ayugram-desktop.nix
+++ maestro_configuration-Icebook/modules/modules-system/nix/binary-caches/ayugram-desktop.nix
@@ -1,12 +1,12 @@
 {
-nix.settings = {
-        substitures = ["https://ayugram-desktop.cachix.org"];
-        trusted-public-keys = [
-            "ayugram-desktop.cachix.org-1:AZ5EqHrJsAKL5YkZYLPEsb1FdD9QlypUwQ0REcJftgA="
-        ];
-    };
-    extra-substitures = ["https://tg-owt.cachix.org"];
-    extra-trusted-public-keys = [
-        "tg-owt.cachix.org-1:lp0BukIhSK3EIyLcDhDZ5zAB0wr8w="        
-    ];
-}
+   nix.settings = {
+     substitures = [
+       "https://ayugram-desktop.cachix.org"
+       "https://tg-owt.cachix.org"
+     ];
+     trusted-public-keys = [
+       "ayugram-desktop.cachix.org-1:AZ5EqHrJsAKL5YkZYLPEsb1FdD9QlypUwQ0REcJftgA="
+       "tg-owt.cachix.org-1:lp0BukIhSK3EIyLcDhDZ5zAB0wr8w="
+     ];
+   };
+}   
