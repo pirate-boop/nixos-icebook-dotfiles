bc2724235f97a4cbc95dba63a2d0e85bde98b6a3
Kori <pirate-boop@users.noreply.github.com>
2026-08-17 01:13:32 +0300

    fix: sync disk changes to flake tree

--- maestro_configuration-Icebook/modules/modules-system/nix/binary-caches/chaotic.nix
+++ maestro_configuration-Icebook/modules/modules-system/nix/binary-caches/chaotic.nix
@@ -1,3 +1,6 @@
 { ... }: {
-  chaotic.nyx.cache.enable=true;
+  nix.settings = {
+    substituters = [ "https://nix.chaotic.cx/nix" ];
+    trusted-public-keys = [ "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" ];
+  };
 }
