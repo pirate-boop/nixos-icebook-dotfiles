8c0e0d638855cd351bc30897186f545111f5cad7
Kori <pirate-boop@users.noreply.github.com>
2026-08-17 02:16:10 +0300

    fix: chaotic

--- maestro_configuration-Icebook/modules/modules-system/nix/binary-caches/chaotic.nix
+++ maestro_configuration-Icebook/modules/modules-system/nix/binary-caches/chaotic.nix
@@ -1,6 +1,3 @@
 { ... }: {
-  nix.settings = {
-    substituters = [ "https://nix.chaotic.cx/nix" ];
-    trusted-public-keys = [ "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" ];
-  };
+ chaotic.nyx.cache.enable = true;
 }
