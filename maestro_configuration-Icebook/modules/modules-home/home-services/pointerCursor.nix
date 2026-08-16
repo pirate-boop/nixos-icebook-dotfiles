398ff3d7cc61748a2ccb6db5417066ba5785405d
Kori <pirate-boop@users.noreply.github.com>
2026-08-17 02:30:49 +0300

    fix: pointerCursor enable explicitly

--- maestro_configuration-Icebook/modules/modules-home/home-services/pointerCursor.nix
+++ maestro_configuration-Icebook/modules/modules-home/home-services/pointerCursor.nix
@@ -1,10 +1,11 @@
 { inputs, pkgs, ... }:
 {
   home.pointerCursor = {
+    enable = true;
     gtk.enable = true;
     x11.enable = true;
     name = "FernBLZ";
     size = 24;
-    package = inputs.anime-cursors.packages.${pkgs.system}.FernBLZ;
+    package = inputs.anime-cursors.packages.${pkgs.stdenv.hostPlatform.system}.FernBLZ;
   };
 }
