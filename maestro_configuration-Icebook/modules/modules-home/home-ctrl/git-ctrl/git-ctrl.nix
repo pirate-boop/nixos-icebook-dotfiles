963d4baec4562244d1e7bff711ed0010d1897951
Kori <pirate-boop@users.noreply.github.com>
2026-08-18 04:33:58 +0300

    sync-myklyqlk

--- maestro_configuration-Icebook/modules/modules-home/home-ctrl/git-ctrl/git-ctrl.nix
+++ maestro_configuration-Icebook/modules/modules-home/home-ctrl/git-ctrl/git-ctrl.nix
@@ -3,7 +3,7 @@
     (pkgs.writeShellApplication {
       name = "git-ctrl";
       text = ''
-        echo "gs    — git status
+        echo "gs    — git status"
         echo "ga    — scan dir"
         echo "gc    — commit with hash (if changes existing "
         echo "gp  — git push main"
