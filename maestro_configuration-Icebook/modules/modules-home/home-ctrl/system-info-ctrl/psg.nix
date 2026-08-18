2e178a3abbab6163e1e6b36dc2ee2cb64aefb5eb
Kori <pirate-boop@users.noreply.github.com>
2026-08-18 11:32:29 +0300

    sync-87a5dd99

--- maestro_configuration-Icebook/modules/modules-home/home-ctrl/system-info-ctrl/psg.nix
+++ maestro_configuration-Icebook/modules/modules-home/home-ctrl/system-info-ctrl/psg.nix
@@ -1,10 +1,10 @@
-# === system-info-ctrl/psg.nix ===
 { pkgs, ... }: {
   home.packages = [
     (pkgs.writeShellApplication {
       name = "psg";
       runtimeInputs = [ pkgs.procps pkgs.gnugrep ];
       text = ''
+        # shellcheck disable=SC2009
         ps aux | grep --color=auto "$@"
       '';
     })
