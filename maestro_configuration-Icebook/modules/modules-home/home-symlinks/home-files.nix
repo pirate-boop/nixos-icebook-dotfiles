6cb3d82e56096d5a3b7af2710b0ff4966187c14c
Kori <pirate-boop@users.noreply.github.com>
2026-08-18 12:55:20 +0300

    sync-cc89bb96

--- maestro_configuration-Icebook/modules/modules-home/home-symlinks/home-files.nix
+++ maestro_configuration-Icebook/modules/modules-home/home-symlinks/home-files.nix
@@ -1,6 +1,6 @@
 { config, ... }: {
-  home.file."removable_devices".source = config.lib.file.mkOutOfStoreSymlink "/run/media/kori";
-  home.file."nix-system_config".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos";
-  home.file."Wallpapers".source = config.lib.file.mkOutOfStoreSymlink 
-  "/etc/nixos/maestro_configuration-Icebook/modules/home/.dotfiles/Wallpapers";
+  #home.file."removable_devices".source = config.lib.file.mkOutOfStoreSymlink "/run/media/kori";
+  home.file."nix-conf".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos";
+  home.file."Wallpapers".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/maestro_configuration-Icebook/modules/home/.dotfiles/Wallpapers";
+  home.file."nvme0n1".source = config.lib.file.mkOutOfStoreSymlink "/mnt/nvme0n1";
 }
